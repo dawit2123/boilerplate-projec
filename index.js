d6696d19a82f2a83da3eba0794d7dfba0ade2bc4
dawit2123 <dawitzewdu2123@gmail.com>
2024-05-02 23:13:53 +0300

    done

--- index.js
+++ index.js
@@ -1,27 +1,48 @@
-var express = require("express");
-var cors = require("cors");
-const multer = require("multer");
-require("dotenv").config();
+// server.js
+// where your node app starts
 
+// init project
+var express = require("express");
 var app = express();
 
-app.use(cors());
-app.use("/public", express.static(process.cwd() + "/public"));
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+const url = require("url");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
-//create a storage
-const upload = multer({ dest: "./public" });
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static("public"));
+
+// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(process.cwd() + "/views/index.html");
+  res.sendFile(__dirname + "/views/index.html");
 });
-// posting a file using multer package
-app.post("/api/fileanalyse", upload.single("upfile"), (req, res, next) => {
-  res.json({
-    name: req.file.originalname,
-    type: req.file.mimetype,
-    size: req.file.size,
-  });
+
+// your first API endpoint...
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
-const port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log("Your app is listening on port " + port);
+
+app.get("/api", (req, res) => {
+  const now = new Date();
+  res.json({ unix: now.getTime(), utc: now.toUTCString() });
+});
+
+app.get("/api/:date", (req, res) => {
+  const paramsDate = req.params.date;
+  const invalidDate = "Invalid Date";
+  const date =
+    parseInt(paramsDate) < 10000
+      ? new Date(paramsDate)
+      : new Date(parseInt(paramsDate));
+
+  date.toString() === invalidDate
+    ? res.json({ error: invalidDate })
+    : res.json({ unix: date.valueOf(), utc: date.toUTCString() });
+});
+
+// listen for requests :)
+var listener = app.listen(3000, function () {
+  console.log("Your app is listening on port " + listener.address().port);
 });
